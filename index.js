4f320bdff9791c404900c2f661bc158f7d834f5e
xiangshouding <fansekey@gmail.com>
2014-09-27 11:24:46 +0800

    bugfix

--- index.js
+++ index.js
@@ -3,19 +3,35 @@
  */
 
 
+function removeFromRequireCache(reg) {
+	for (var path in require.cache) {
+		if (reg.test(path)) {
+			delete(require.cache[path]);
+		}
+	}
+}
+
+
 module.exports = function (content, file, settings) {
+	
+
+	removeFromRequireCache(/asciidoctor\.js/); //not do cache, fix a bug of asciidoctor.js
+
 	var asciidoctor = require('asciidoctor.js')();
+
 	var opal = asciidoctor.Opal
 	var processor = null;
 
 	if (settings.useExtensions) {
-		processor = asciidoctor.Asciidoctor(true); (3)
+		processor = asciidoctor.Asciidoctor(true); 
 	} else {
-		processor = asciidoctor.Asciidoctor(); (4)
+		processor = asciidoctor.Asciidoctor();
 	}
+
 	var options = opal.hash2(
 	    settings.mod,
 	    settings.opt);
+
 	var html = processor.$convert(content, options);
 	return html;
 
